e5938e3d7a1fcb5f67fab3a3d9160e5e73af1401
larbert <17835126760@163.com>
2022-08-24 21:51:32 +0800

    session

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/larbert/mayflyorm
+
+go 1.18
+
+require github.com/larbert/mayflylog v0.0.0-20220818111313-adcd2e92f970 // indirect
--- session/raw.go
+++ session/raw.go
@@ -1,7 +1,44 @@
 package session
 
-import "database/sql"
+import (
+	"database/sql"
+	"strings"
+
+	"github.com/larbert/mayflylog"
+)
 
 type Session struct {
-	db *sql.DB
+	db        *sql.DB
+	sql       strings.Builder
+	sqlParams []interface{}
+}
+
+func New(db *sql.DB) Session {
+	return Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlParams = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlParams = append(s.sqlParams, values...)
+	return s
+}
+
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	mayflylog.Info(s.sql.String(), s.sqlParams)
+	result, err = s.DB().Exec(s.sql.String(), s.sqlParams...)
+	if err != nil {
+		mayflylog.Error(err)
+	}
+	return
 }
