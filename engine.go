12f8b36e2f7fa2401360186ce768201b179334f2
larbert <17835126760@163.com>
2022-08-25 21:14:54 +0800

    engine-base

---
+++ engine.go
@@ -0,0 +1,35 @@
+package mayflyorm
+
+import (
+	"database/sql"
+
+	"github.com/larbert/mayflylog"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func New(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		mayflylog.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		mayflylog.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	mayflylog.Info("数据库连接成功")
+	return
+}
+
+func (e *Engine) Close() {
+	err := e.db.Close()
+	if err != nil {
+		mayflylog.Error(err)
+		return
+	}
+	mayflylog.Info("数据库关闭成功")
+}
